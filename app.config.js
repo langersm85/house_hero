4d50cb4de018670b68fbc15052f5846540c323bd
Matt Lang <langersm@gmail.com>
2026-01-05 13:48:55 +1030

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = ({ config }) => {
+  const iconPath = path.resolve(__dirname, "assets/images/app-icon.png");
+
+  return {
+    ...config,
+    icon: iconPath,
+    android: {
+      ...(config.android || {}),
+      adaptiveIcon: {
+        ...(config.android?.adaptiveIcon || {}),
+        foregroundImage: iconPath,
+        backgroundColor: "#000000",
+      },
+    },
+  };
+};
