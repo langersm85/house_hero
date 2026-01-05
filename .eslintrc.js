383afe1c603dd1179068df602427b17800e212cb
natively.dev <api@natively.dev>
2026-01-05 04:07:21 +0000

    fix: Use npx expo in package.json scripts to resolve build issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,31 @@
-// https://docs.expo.dev/guides/using-eslint/
+
 module.exports = {
-  extends: [
-    'expo',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
-  ],
+  extends: ['expo', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'import'],
-  root: true,
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  ignorePatterns: ['/dist/*', '/public/*', '/babel-plugins/*'],
-  env: {
-    browser: true,
-  },
   rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/prefer-as-const": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/no-wrapper-object-types": "off",
-    "@typescript-eslint/ban-tslint-comment": "off",
-    "react/no-unescaped-entities": "off",
-    "import/no-unresolved": "error",
-    "prefer-const": "off",
-    "react/prop-types": 1,
-    "no-case-declarations": "off",
-    "no-empty": "off",
-    "react/display-name": "off",
-    "no-var": "off"
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'never',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
   },
   overrides: [
     {
-      files: ['metro.config.js'],
+      files: ['*.config.js', 'app.config.js', 'babel.config.js', 'metro.config.js'],
+      env: {
+        node: true,
+      },
       rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    }
-  ]
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['node_modules/', '.expo/', 'dist/', 'build/'],
 };
